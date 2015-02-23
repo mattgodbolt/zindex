3943574c993d1788f96a05cd829da5346fbe7603
Matt Godbolt <mgodbolt@drwholdings.com>
2015-02-23 17:56:15 -0600

    Initial import

---
+++ src/zindex.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : zindex.cpp
+ Author      : Matt Godbolt
+ Version     :
+ Copyright   : (c) 2015 Matt Godbolt
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
