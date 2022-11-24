ba13d7872bc12e78290b6f39549ab37a796eeede
sbendiola <steve.bendiola@gmail.com>
2022-11-24 17:46:49 -0600

    Revert "add group pattern parts"
    This reverts commit e99055ab77d950bf5876cb3b04fc13eb37fadd99.

--- src/RegExpIndexer.h
+++ src/RegExpIndexer.h
@@ -10,7 +10,7 @@
 class RegExpIndexer : public LineIndexer {
     RegExp re_;
     unsigned int captureGroup_;
-    std::vector<std::tuple<std::string, uint>> captureFormatParts_;
+    std::string captureFormat_;
 
 public:
     explicit RegExpIndexer(const std::string &regex);
@@ -21,6 +21,4 @@ public:
 private:
     void onMatch(IndexSink &sink, const std::string &line, size_t offset,
                  const RegExp::Match &match);
-    void onMatch(IndexSink &sink, const std::string &line,
-                            size_t offset, const RegExp::Matches &result);
 };
